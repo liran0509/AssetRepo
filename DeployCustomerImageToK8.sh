3d61cbcf4da57642c58a1b418486900b372f0755
liran0509 <57391885+liran0509@users.noreply.github.com>
2020-08-03 13:43:49 +0300

    Update DeployCustomerImageToK8.sh

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -2,5 +2,5 @@
 #
 
 cd /opt/rancher
-./rancher login https://k8s.eur.ad.sag/v3 --token token-vzcht:b6drnslrqsf2cnwnxr8jxp572zqkj4fl5hzxjq94tgl22qh4xdq2r9
-./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
+./rancher login https://k8s.eur.ad.sag/v3 --token token-z6wcr:w58smtj2pnntw5jmtzmht7czgds7nms7r9wsjnbrrmzssmxbrv2lpw
+./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml
